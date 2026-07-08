37ba2b10842b02f61ac7e2ffc8c81c53fccada04
Your Name <you@example.com>
2026-07-08 23:52:19 +0530

    solving error

--- backend/src/db/db_connect.js
+++ backend/src/db/db_connect.js
@@ -1,22 +1,8 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import { DB_NAME } from "../constants.js";
 
-// dotenv.config();
 
-// const dbConnect = async () => {
-//   try {
-//     const connectionInstance = await mongoose.connect(`process.env.MONGODB_URI, {
-//       dbName: DB_NAME,
-//     }`);
-//     console.log(`\nMongoDB connected !! DB host : ${connectionInstance.connection.host}`);
-//   } catch (error) {
-//     console.log("Mongodb connection error", error);
-//     process.exit(1);
-//   }
-// };
 
-// export default dbConnect;
+
+
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { DB_NAME } from "../constants.js";
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,15 +7,20 @@ dotenv.config({
   path: "../.env",
 });
 
+ 
 const app = express();
 
 
 
 const startServer = async () => {
-  await dbConnect();
-  app.listen(process.env.PORT || 3000, () => {
+  await dbConnect().then(()=>{
+    app.listen(process.env.PORT || 3000, () => {
     console.log(`App is listening on port ${process.env.PORT || 3000}`);
-  });
+  })
+  }).catch((err)=>{
+    console.log("MongoDB connection failed !! ",err);
+  })
+  
 };
 
 startServer();
