ea8d5e881813d26ffead18259d1df79a7dba3ab5
Your Name <you@example.com>
2026-07-20 13:17:25 +0530

    cloudinary

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+
+        if(!localFilePath){
+            return "Select your file"
+        }
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        
+        // if file is uploaded on cloudinary succesfully
+
+        console.log("file is uploaded succesfully on cloudinary",response.url);
+    } catch (error) {
+        
+    }
+}
