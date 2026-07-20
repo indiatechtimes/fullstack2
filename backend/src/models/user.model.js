88a1f1b1dce10fdfd9fa3476186e51295f245015
Your Name <you@example.com>
2026-07-20 11:33:45 +0530

    jwt tokens

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
 const userSchema=new mongoose.Schema({
 
     id:{
@@ -54,6 +58,50 @@ const userSchema=new mongoose.Schema({
     },
     
 },{timestamps:true});
+
+userSchema.pre("save", async function (next) {
+
+    if(this.isModified("password")){
+        this.password=bcrypt.hash(this.password,10)
+    next()
+    }
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+
+    return jwt.sign(
+        {
+            _id:this._id,
+            id:this.id,
+            email:this.email,
+            userName:this.userName,
+            fullName:this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function (){
+
+    return jwt.sign(
+        {
+            //_id:this._id
+            id:this.id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 export const user=mongoose.model("user",userSchema);
