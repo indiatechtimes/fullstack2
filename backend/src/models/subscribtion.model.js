2e6adc1906399370d0ccf4f7c51aaea748eb9fd7
Your Name <you@example.com>
2026-08-14 18:51:35 +0530

    add subscription model: define schema for subscriber and channel

---
+++ backend/src/models/subscribtion.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+    {
+
+        subscriber: {
+            type: Schema.Types.ObjectId,// one who is subscribing
+            ref: "User"
+        },
+        channel: {
+            type: Schema.Types.ObjectId,//one to whome "subscriber is subscribing"
+
+            ref: "User"
+        }
+
+
+
+
+    }, { timestamps: true });
+
+
+export const subscription = mongoose.model("subscription", subscriptionSchema);
